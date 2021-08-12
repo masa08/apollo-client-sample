070b23293e48f2d0d6a6b55cca5daf60a12a8fdb
masa08 <ushimasarug08@gmail.com>
2021-08-12 18:16:46 +0900

    :memo: memo

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import {
   gql,
 } from "@apollo/client";
 
+// clientの初期化
 const client = new ApolloClient({
   uri: "https://48p1r2roz4.sse.codesandbox.io",
   cache: new InMemoryCache(),
@@ -42,6 +43,7 @@ function ExchangeRates() {
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* reactでapollo clientを使用できるようにする */}
     <ApolloProvider client={client}>
       <ExchangeRates />
     </ApolloProvider>
@@ -53,3 +55,12 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// 状態管理に関して
+// inMemoryCacheを用いれば、一定の条件を満たさない限り、cacheにデータを取りに行くのでそれで管理可能。
+// reactive variablesなどを使うことによって、clientでも柔軟な状態管理が可能。
+// readQuery, qriteQueryを使うとcacheと直接やり取りをすることもできる。
+
+// apollo clientにおける状態管理が腑に落ちてなかったのだが、
+// 結局readCacheもuseQuery(network-onlyでなくかつ2回目以降の発行)もcacheにデータを取りにいってて
+// そういう意味ではやっていることは同じなのか、あとはreactive variablesでもっと柔軟に状態管理できると。
